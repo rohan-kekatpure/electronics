251b62a74f15f9151ce1eb2dcc60c1877809af12
rohankekatpure <rohankekatpure@wealthfront.com>
2024-09-03 11:49:15 -0500

    IR controlled rover

---
+++ wifi_and_ir_remote_controlled_rover/ircodes.h
@@ -0,0 +1,71 @@
+#ifndef _IRCODES
+#define _IRCODES
+
+struct IRCODES {
+  // ELEGOO remote codes
+  static const int ELEGOO_BTN_UP = 0xFF629D;
+  static const int ELEGOO_BTN_DOWN = 0xFFA857;
+  static const int ELEGOO_BTN_LEFT = 0xFF22DD;
+  static const int ELEGOO_BTN_RIGHT = 0xFFC23D;
+  static const int ELEGOO_BTN_OK = 0xFF02FD;
+  static const int ELEGOO_BTN_1 = 0xFF6897;
+  static const int ELEGOO_BTN_2 = 0xFF9867;
+  static const int ELEGOO_BTN_3 = 0xFFB04F;
+  static const int ELEGOO_BTN_4 = 0xFF30CF;
+  static const int ELEGOO_BTN_5 = 0xFF18E7;
+  static const int ELEGOO_BTN_6 = 0xFF7A85;
+  static const int ELEGOO_BTN_7 = 0xFF10EF;
+  static const int ELEGOO_BTN_8 = 0xFF38C7;
+  static const int ELEGOO_BTN_9 = 0xFF5AA5;
+  static const int ELEGOO_BTN_0 = 0xFF4AB5;
+  static const int ELEGOO_BTN_STAR = 0xFF42BD;
+  static const int ELEGOO_BTN_HASH = 0xFF52AD;
+
+  // LG TV remote codes
+  static const int LG_BTN_ENERGY = 0x20DFA956;
+  static const int LG_BTN_POWER = 0x20DF10EF;
+  static const int LG_BTN_TV = 0x20DFF00F;
+  static const int LG_BTN_AVMODE = 0x20DF0CF3;
+  static const int LG_BTN_INFO = 0x20DF55AA;
+  static const int LG_BTN_RATIO = 0x20DF9E61;
+  static const int LG_BTN_INPUT = 0x20DFD02F;
+  static const int LG_BTN_1 = 0x20DF8877;
+  static const int LG_BTN_2 = 0x20DF48B7;
+  static const int LG_BTN_3 = 0x20DFC837;
+  static const int LG_BTN_4 = 0x20DF28D7;
+  static const int LG_BTN_5 = 0x20DFA857;
+  static const int LG_BTN_6 = 0x20DF6897;
+  static const int LG_BTN_7 = 0x20DFE817;
+  static const int LG_BTN_8 = 0x20DF18E7;
+  static const int LG_BTN_9 = 0x20DF9867;
+  static const int LG_BTN_0 = 0x20DF08F7;
+  static const int LG_BTN_DASH = 0x20DF32CD;
+  static const int LG_BTN_FLASHBK = 0x20DF58A7;
+  static const int LG_BTN_SETTINGS = 0x20DFC23D;
+  static const int LG_BTN_QMENU = 0x20DFA25D;
+  static const int LG_BTN_LEFT_ARROW = 0x20DFE01F;
+  static const int LG_BTN_RIGHT_ARROW = 0x20DF609F;
+  static const int LG_BTN_UP_ARROW = 0x20DF02FD;
+  static const int LG_BTN_DOWN_ARROW = 0x20DF827D;
+  static const int LG_BTN_OK = 0x20DF22DD;
+  static const int LG_BTN_UNDO = 0x20DF14EB;
+  static const int LG_BTN_EXIT = 0x20DFDA25;
+  static const int LG_BTN_VOL_UP = 0x20DF40BF;
+  static const int LG_BTN_VOL_DOWN = 0x20DFC03F;
+  static const int LG_BTN_CHANNEL_UP = 0x20DF00FF;
+  static const int LG_BTN_CHANNEL_DOWN = 0x20DF807F;
+  static const int LG_BTN_FAV = 0x20DF7887;
+  static const int LG_BTN_MUTE = 0x20DF906F;
+  static const int LG_BTN_SIMPLINK = 0x20DF7E81;
+  static const int LG_BTN_RED_ = 0x20DF4EB1;
+  static const int LG_BTN_GREEN = 0x20DF4EB1;
+  static const int LG_BTN_YELLOW = 0x20DF4EB1;
+  static const int LG_BTN_BLUE = 0x20DF4EB1;
+  static const int LG_BTN_STOP = 0x20DF8D72;
+  static const int LG_BTN_PLAY = 0x20DF0DF2;
+  static const int LG_BTN_PAUSE = 0x20DF5DA2;
+  static const int LG_BTN_REWIND = 0x20DFF10E;
+  static const int LG_BTN_FORWARD = 0x20DF718E;
+};
+
+#endif
---
+++ wifi_and_ir_remote_controlled_rover/main.ino
@@ -0,0 +1,306 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPClient.h>
+#include <LittleFS.h>
+#include <Arduino_JSON.h>
+
+#include <Arduino.h>
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRutils.h>
+
+#include "ircodes.h"
+
+// Webserver constants
+const char* SSID = "Kekatpure_home";
+const char* PASSWD = "628OldStoneWifi";
+const uint PWM = 255;
+double GLOBAL_LEFT_SPEED = 100.0;
+double GLOBAL_RIGHT_SPEED = 100.0;
+const uint8_t LEFT_SIDE_CONTROL_PIN = D1;
+const uint8_t RIGHT_SIDE_CONTROL_PIN = D2;
+
+// IR Remote control constants 
+const uint16_t IR_RECV_PIN = D3;
+IRrecv irrecv(IR_RECV_PIN);
+decode_results IR_RESULT;
+const uint IR_MIN_LEVEL = 0;
+const uint IR_MAX_LEVEL = 9;
+const double MIN_SPEED = 0;
+const double MAX_SPEED = 255;
+const double LEVEL2SPEED = double(MAX_SPEED - MIN_SPEED) / double(IR_MAX_LEVEL - IR_MIN_LEVEL);  
+const double SPEED_DELTA = LEVEL2SPEED;
+
+
+ESP8266WebServer server(80);
+
+void setLeftSpeed(const double value) {
+  analogWrite(LEFT_SIDE_CONTROL_PIN, value);
+}
+
+void setRightSpeed(const double value) {
+  analogWrite(RIGHT_SIDE_CONTROL_PIN, value);
+}
+
+void setSpeed(const double leftSpeed, const double rightSpeed) {
+  setLeftSpeed(leftSpeed);
+  setRightSpeed(rightSpeed);
+}
+
+void serveIndex() {
+  if (! LittleFS.begin()) {
+    Serial.println("Error mounting LittleFS");    
+  }
+  server.serveStatic("/", LittleFS, "/index.html");  
+}
+
+void handlexy() {
+  JSONVar coords = JSON.parse(server.arg(0));
+  
+  if (JSON.typeof(coords) == "undefined"){
+    Serial.println(server.arg(0));
+    Serial.println("Failed to parse request payload");
+    return;
+  }
+
+  // Received from the front end
+  double x = coords["x"];
+  double y = coords["y"];
+
+  // Derived parameters
+  double r = sqrt(x * x + y * y);
+  double theta_rad = PI / 2.0 - atan2(y, x);
+  double theta_deg = theta_rad * RAD_TO_DEG;
+
+  // Conversion to motor PWMs
+  double base_speed = r; // speed is distance from the center
+  double left_speed = base_speed;
+  double right_speed = base_speed;
+  double factor = 1.0 - fabs(sin(theta_rad));
+
+  if ((theta_deg >= 0.0) && (theta_deg <= 90.0)) {
+    right_speed *= factor;    
+  } else if ((theta_deg < 0.0) && (theta_deg >= -90.0)) {
+    left_speed *= factor;
+  }
+
+  // Convert speeds to PWM
+  left_speed *= PWM;
+  right_speed *= PWM; 
+
+  // Change speed state
+  GLOBAL_LEFT_SPEED = left_speed;
+  GLOBAL_RIGHT_SPEED = right_speed;  
+
+  String fmt = "LEFT_SPEED = %0.2f, RIGHT_SPEED = %0.2f\n";
+  Serial.printf(fmt.c_str(), GLOBAL_LEFT_SPEED, GLOBAL_RIGHT_SPEED);
+
+  // Write to Drive pins
+  setSpeed(GLOBAL_LEFT_SPEED, GLOBAL_RIGHT_SPEED);
+}
+
+void IRLeftTurn() {
+  setLeftSpeed(MIN_SPEED);
+  delay(500);
+  setLeftSpeed(GLOBAL_LEFT_SPEED);
+}
+
+void IRRightTurn() {
+  setRightSpeed(MIN_SPEED);
+  delay(500);
+  setRightSpeed(GLOBAL_RIGHT_SPEED);
+}
+
+void IRSetSpeed(uint level) {
+  if ((level < IR_MIN_LEVEL) || (level > IR_MAX_LEVEL)) {
+    return;
+  }
+
+  double speed = ceil(MIN_SPEED + level * LEVEL2SPEED);  
+  setSpeed(speed, speed);
+}
+
+void IRStopCar() {
+  IRSetSpeed(0);
+}
+
+void IRIncrSpeed() {
+  double newSpeed = GLOBAL_LEFT_SPEED + SPEED_DELTA;
+  GLOBAL_LEFT_SPEED = min(MAX_SPEED, newSpeed);
+  GLOBAL_RIGHT_SPEED = GLOBAL_LEFT_SPEED;
+  setSpeed(GLOBAL_LEFT_SPEED, GLOBAL_RIGHT_SPEED);
+}
+
+void IRDecrSpeed() {
+  double newSpeed = GLOBAL_LEFT_SPEED - SPEED_DELTA;
+  GLOBAL_LEFT_SPEED = max(MIN_SPEED, newSpeed);
+  GLOBAL_RIGHT_SPEED = GLOBAL_LEFT_SPEED;
+  setSpeed(GLOBAL_LEFT_SPEED, GLOBAL_RIGHT_SPEED);
+}
+
+void handleIR() {
+  if (! irrecv.decode(&IR_RESULT)) { 
+    return;
+  }   
+  switch (IR_RESULT.value) {
+
+    // Left turn
+    case IRCODES::LG_BTN_LEFT_ARROW:
+    case IRCODES::ELEGOO_BTN_LEFT:
+      Serial.println("BUTTON LEFT");
+      IRLeftTurn();
+      break;
+    
+    // Right turn
+    case IRCODES::LG_BTN_RIGHT_ARROW:
+    case IRCODES::ELEGOO_BTN_RIGHT:
+      Serial.println("BUTTON RIGHT");
+      IRRightTurn();
+      break;
+
+    // Increase speed
+    case IRCODES::LG_BTN_UP_ARROW:
+    case IRCODES::ELEGOO_BTN_UP:
+      Serial.println("BUTTON UP");
+      IRIncrSpeed();
+      break;
+    
+    // Decrease speed
+    case IRCODES::LG_BTN_DOWN_ARROW:
+    case IRCODES::ELEGOO_BTN_DOWN:
+      Serial.println("BUTTON DOWN");
+      IRDecrSpeed();
+      break;
+
+    // Stop the car
+    case IRCODES::LG_BTN_OK:
+    case IRCODES::ELEGOO_BTN_OK:
+      Serial.println("BUTTON OK");
+      IRStopCar();
+      break;
+
+    // Speed setting 1
+    case IRCODES::LG_BTN_1:
+    case IRCODES::ELEGOO_BTN_1:
+      Serial.println("BUTTON 1");
+      IRSetSpeed(1);
+      break;
+
+    // Speed setting 2
+    case IRCODES::LG_BTN_2:
+    case IRCODES::ELEGOO_BTN_2:
+      Serial.println("BUTTON 2");
+      IRSetSpeed(2);
+      break;
+
+    // Speed setting 3
+    case IRCODES::LG_BTN_3:
+    case IRCODES::ELEGOO_BTN_3:
+      Serial.println("BUTTON 3");
+      IRSetSpeed(3);
+      break;
+
+    // Speed setting 4
+    case IRCODES::LG_BTN_4:
+    case IRCODES::ELEGOO_BTN_4:
+      Serial.println("BUTTON 4");
+      IRSetSpeed(4);
+      break;
+
+    // Speed setting 5
+    case IRCODES::LG_BTN_5:
+    case IRCODES::ELEGOO_BTN_5:
+      Serial.println("BUTTON 5");
+      IRSetSpeed(5);
+      break;
+
+    // Speed setting 6
+    case IRCODES::LG_BTN_6:
+    case IRCODES::ELEGOO_BTN_6:
+      Serial.println("BUTTON 6");
+      IRSetSpeed(6);
+      break;
+
+    // Speed setting 7
+    case IRCODES::LG_BTN_7:
+    case IRCODES::ELEGOO_BTN_7:
+      Serial.println("BUTTON 7");
+      IRSetSpeed(7);
+      break;
+
+    // Speed setting 8
+    case IRCODES::LG_BTN_8:
+    case IRCODES::ELEGOO_BTN_8:
+      Serial.println("BUTTON 8");
+      IRSetSpeed(8);
+      break;
+
+    // Speed setting 9
+    case IRCODES::LG_BTN_9:
+    case IRCODES::ELEGOO_BTN_9:
+      Serial.println("BUTTON 9");
+      IRSetSpeed(9);
+      break;
+
+    // Stop the car
+    case IRCODES::LG_BTN_0:
+    case IRCODES::ELEGOO_BTN_0:
+      Serial.println("BUTTON 0");
+      IRStopCar();
+      break;
+
+    // Stop the car
+    case IRCODES::ELEGOO_BTN_STAR:
+      Serial.println("BUTTON STAR");
+      IRStopCar();
+      break;
+
+    // Stop the car
+    case IRCODES::LG_BTN_POWER:
+    case IRCODES::ELEGOO_BTN_HASH:
+      Serial.println("BUTTON HASH");
+      IRStopCar();
+      break;
+
+    default:
+      Serial.println("OTHER or UNKNOWN BUTTON");
+      break;
+  }  
+  irrecv.resume();  // Receive the next value
+  delay(100);  
+}
+
+void setup() {     
+  // Pin Setup  
+  pinMode(LEFT_SIDE_CONTROL_PIN, OUTPUT);
+  pinMode(RIGHT_SIDE_CONTROL_PIN, OUTPUT);
+
+  Serial.begin(115200);
+
+  // Connect Wifi
+  Serial.printf("Connecting to: %s\n", SSID);
+  WiFi.begin(SSID, PASSWD);  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Connected");
+  Serial.println(WiFi.localIP()); 
+
+  // Setup of IR remote control
+  irrecv.enableIRIn();  
+
+  // Start Webserver
+  serveIndex();
+  server.on("/xy", handlexy);
+  server.begin();  
+}
+
+void loop() {
+  // server.handleClient();
+  handleIR();
+}
